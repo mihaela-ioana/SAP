2a794d9abc15beb87f10c97f9504e1f5aae1499a
M <bmaicel@yahoo.com>
2020-05-29 22:14:33 +0300

    added i18n, update, checkout and list of borrowed

--- webapp/controller/BookList.controller.js
+++ webapp/controller/BookList.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/core/Fragment"
- ], function (Controller, MessageToast, Fragment) {
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+   "sap/ui/model/resource/ResourceModel"
+ ], function (Controller, MessageToast, Fragment, Filter, FilterOperator, ResourceModel) {
     "use strict";
     return Controller.extend("org.ubb.books.controller.BookList", {
+
+        onInit : function () {
+            // set i18n model on view
+            var i18nModel = new ResourceModel({
+               bundleName: "org.ubb.books.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
+         },
 
         onBookDialogOpen: function(event) {
             var oView = this.getView();
@@ -17,7 +28,6 @@ sap.ui.define([
 				}).then(function (oDialog) {
 					// connect dialog to the root view of this component (models, lifecycle)
                     oView.addDependent(oDialog);
-                    this.insertInputData();
 					oDialog.open();
 				});
 			} else {
@@ -48,7 +58,6 @@ sap.ui.define([
             var oItem = this.getView().byId("idBooksTable").getSelectedItem();
 
             if (oItem !== null){
-                console.log("I was selected!");
                 var oEntry = oItem.getBindingContext().getObject();
                 this.getView().byId('newISBN').setValue(oEntry.ISBN);
                 this.getView().byId('newTitle').setValue(oEntry.Title);
@@ -56,9 +65,15 @@ sap.ui.define([
                 this.getView().byId('newLanguage').setValue(oEntry.Language);
                 this.getView().byId('newPublishDate').setValue(oEntry.PublishDate);
                 this.getView().byId('newNbAvailableBooks').setValue(oEntry.NbAvailableBooks);
-
+                this.getView().byId('newTotalNbBooks').setValue(oEntry.TotalNbBooks);
             } else {
-                console.log("I was not selected!");
+                this.getView().byId('newISBN').setValue("");
+                this.getView().byId('newTitle').setValue("");
+                this.getView().byId('newAuthor').setValue("");
+                this.getView().byId('newLanguage').setValue("");
+                this.getView().byId('newPublishDate').setValue("");
+                this.getView().byId('newNbAvailableBooks').setValue("");
+                this.getView().byId('newTotalNbBooks').setValue("");
             }
 
             
@@ -79,6 +94,7 @@ sap.ui.define([
             var language = this.getView().byId('newLanguage').getValue();
             var publish = this.getView().byId('newPublishDate').getValue();
             var nbAv = this.getView().byId('newNbAvailableBooks').getValue();
+            var totalNbAv = this.getView().byId('newTotalNbBooks').getValue();
 
             var oEntry = {
                 ISBN: isbn,
@@ -86,17 +102,24 @@ sap.ui.define([
                 Author: author,
                 Language: language,
                 PublishDate: publish,
-                NbAvailableBooks: nbAv
+                NbAvailableBooks: nbAv,
+                TotalNbBooks: totalNbAv
             };
+
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("addBookSuccess", [sRecipient]);
+            var sMsgError = oBundle.getText("addBookError", [sRecipient]);
             
             this.getView().getModel().create("/Books", oEntry, {
                 success: () => {
-                    MessageToast.show("Book added sucessfully!");
+                    MessageToast.show(sMsg);
                 },
                 error: () => {
-                    MessageToast.show("Book could not be added!");
+                    MessageToast.show(sMsgError);
                 }
             });
+            this.onCloseDialog();
             
         },
 
@@ -107,6 +130,7 @@ sap.ui.define([
             var language = this.getView().byId('newLanguage').getValue();
             var publish = this.getView().byId('newPublishDate').getValue();
             var nbAv = this.getView().byId('newNbAvailableBooks').getValue();
+            var totalNbAv = this.getView().byId('newTotalNbBooks').getValue();
 
             var oEntry = {
                 ISBN: isbn,
@@ -114,30 +138,42 @@ sap.ui.define([
                 Author: author,
                 Language: language,
                 PublishDate: publish,
-                NbAvailableBooks: nbAv
+                NbAvailableBooks: nbAv,
+                TotalNbBooks: totalNbAv
             };
+
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("updateBookSuccess", [sRecipient]);
+            var sMsgError = oBundle.getText("updateBookError", [sRecipient]);
             
             var sPath = this.byId("idBooksTable").getSelectedContexts()[0].getPath();
             this.getView().getModel().update(sPath, oEntry, {
                 success: () => {
-                    MessageToast.show("Book updated sucessfully!");
+                    MessageToast.show(sMsg);
                 },
                 error: () => {
-                    MessageToast.show("Book could not be updated!");
+                    MessageToast.show(sMsgError);
                 }
             });
+            this.onCloseDialog();
         },
        
         onDeleteBook(oEvent) {
             const aSelContexts = this.byId("idBooksTable").getSelectedContexts();
             const sPathToBook = aSelContexts[0].getPath();
 
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("deleteBookSuccess", [sRecipient]);
+            var sMsgError = oBundle.getText("deleteBookError", [sRecipient]);
+
             this.getView().getModel().remove(sPathToBook, {
                 success: () => {
-                    MessageToast.show("Book deleted sucessfully!");
+                    MessageToast.show(sMsg);
                 },
                 error: () => {
-                    MessageToast.show("Book could not be deleted!");
+                    MessageToast.show(sMsgError);
                 }
             });
         },
@@ -154,7 +190,66 @@ sap.ui.define([
 				this._oSmartTable = this.getView().byId("idBooksTable");
 			}
 			return this._oSmartTable;
-		},
+        },
+        
+        onFilterBorrowedBooks: function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				var oFilter1 = new sap.ui.model.Filter("Firstname", sap.ui.model.FilterOperator.Contains, sQuery);
+                var oFilter2 = new sap.ui.model.Filter("Lastname", sap.ui.model.FilterOperator.Contains, sQuery);
+                var oFilter3 = new sap.ui.model.Filter("Title", sap.ui.model.FilterOperator.Contains, sQuery);
+                var oFilter4 = new sap.ui.model.Filter("Author", sap.ui.model.FilterOperator.Contains, sQuery);
+                var allFilter = new sap.ui.model.Filter([oFilter1, oFilter2, oFilter3, oFilter4], false); 
+                var oTable = this.getView().byId("idBorrowedTable");
+                var oBinding = oTable.getBinding("items");
+                oBinding.filter(allFilter);
+			}
+
+			// filter binding
+			// var oTable = this.byId("idBorrowedTable");
+			// var oBinding = oEvent.getSource().getBinding("items");
+			// oBinding.filter(aFilter);
+        },
+        
+        onBorrowedBook: function(oEvent) {
+            var itemPosition = oEvent.getSource().getBindingContext().getObject();
+            var isbn = itemPosition.ISBN;
+
+            var oEntry = {
+                Id: "1",
+                Firstname: "",
+                Lastname: "",
+                Username: "",
+                CheckoutDate: itemPosition.PublishDate,
+                ReturnDate: itemPosition.PublishDate,
+                ISBN: isbn,
+                Title: "",
+                Author: ""
+            };
+
+            if(itemPosition.NbAvailableBooks == "00000"){
+                MessageToast.show("There are no more books to check out!");
+            } else {
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                var sMsg = oBundle.getText("addCheckoutSuccess", [sRecipient]);
+                var sMsgError = oBundle.getText("addCheckoutError", [sRecipient]);
+
+                this.getView().getModel().create("/BorrowedBooks", oEntry, {
+                    success: () => {
+                        MessageToast.show(sMsg);
+                        this.byId("idBooksTable").getBinding("items").refresh();
+                        this.byId("idBorrowedTable").getBinding("items").refresh();
+                    },
+                    error: () => {
+                        MessageToast.show(sMsgError);
+                    }
+                });
+            }
+        }
 
     });
  });
